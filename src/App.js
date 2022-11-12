f601f1459f56efd194138f6c0242b3a819e4eede
Anirudh A V <anirudh.av02@gmail.com>
2022-11-12 13:24:56 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
+import PostsList from './features/posts/PostsList';
+import AddPostForm from './features/posts/AddPostForm';
 import './App.css';
 
 function App() {
   return (
     <main className="App">
-
+      <AddPostForm />
+      <PostsList />
     </main>
   );
 }
